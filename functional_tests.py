749a3ae823787514d6263643341e5fc6b21bbd6e
objectisliper <objectisliper@gmail.com>
2019-07-25 14:10:15 +0300

    Init

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """ New user test """
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self) -> None:
+        # User quit site
+
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Юзер заходит на сайт
+
+        self.browser.get('http://localhost:8000')
+
+        # Check title
+
+        self.assertIn('To-Do', self.browser.title)
+
+        self.fail('Закончить тест!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
