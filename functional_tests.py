d3dd336bcf70136c4fba8e9e89b4f5ed759b7487
objectisliper <objectisliper@gmail.com>
2019-07-29 14:45:11 +0300

    Init

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -21,6 +23,40 @@ class NewVisitorTest(unittest.TestCase):
         # Check title
 
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+
+        self.assertIn('To-Do', header_text)
+
+        # Find input field
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Enter a name of point
+
+        inputbox.send_keys('Buy smthng')
+
+        # Save changes
+
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        # check is saved
+
+        table = self.browser.find_element_by_id('id_list_table')
+
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertTrue(
+            any(row.text == 'Buy smthng' for row in rows),
+            'Новый элемент списка не появился в таблице'
+        )
 
         self.fail('Закончить тест!')
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
+from django.http import HttpRequest
 
 
 class SmokeTest(TestCase):
@@ -9,4 +10,12 @@ class SmokeTest(TestCase):
         found = resolve('/')
 
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        html = response.content.decode('utf8')
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTrue(html.strip().startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
 
