06bc3dbf626892f1a7348261b7978d67f69eb162
objectisliper <objectisliper@gmail.com>
2019-08-01 21:48:40 +0300

    redeclare

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,52 +5,32 @@ from functools import wraps
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.remote.command import Command
+from selenium.webdriver.common.by import By
 
 from django.test import LiveServerTestCase
 
 
-def wait_for_element_to_found(time_to_wait: int = 10):
-    def inner_function(function):
-        @wraps(function)
-        def wrapper(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    function(*args, **kwargs)
-                    return
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > time_to_wait:
-                        raise e
-                    time.sleep(0.5)
-        return wrapper
-    return inner_function
+class ChromeWebdriver(webdriver.Chrome):
 
-class Browser(webdriver.Chrome):
+    time_to_wait = 10
 
-    @wait_for_element_to_found()
     def find_element(self, by=By.ID, value=None):
-        if self.w3c:
-            if by == By.ID:
-                by = By.CSS_SELECTOR
-                value = '[id="%s"]' % value
-            elif by == By.TAG_NAME:
-                by = By.CSS_SELECTOR
-            elif by == By.CLASS_NAME:
-                by = By.CSS_SELECTOR
-                value = ".%s" % value
-            elif by == By.NAME:
-                by = By.CSS_SELECTOR
-                value = '[name="%s"]' % value
-        return self.execute(Command.FIND_ELEMENT, {
-            'using': by,
-            'value': value})['value']
+        start_time = time.time()
+        while True:
+            try:
+                return super().find_element(by, value)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > self.time_to_wait:
+                    raise e
+                time.sleep(0.5)
 
 
 class NewVisitorTest(LiveServerTestCase):
     """ New user test """
 
     def setUp(self) -> None:
-        self.browser = webdriver.Chrome()
+        self.browser = ChromeWebdriver()
 
     def tearDown(self) -> None:
         # User quit site
@@ -64,7 +44,6 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.assertIn(row_text, [row.text for row in rows])
 
-    @wait_for_element_to_found()
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Юзер заходит на сайт
 
@@ -95,8 +74,6 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys(Keys.ENTER)
 
-        time.sleep(1)
-
         # Enter a name of point
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -113,7 +90,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.wait_for_row_in_list_table('2: Buy smthng else')
 
-
+        self.fail('looolz')
 
 
 if __name__ == '__main__':
