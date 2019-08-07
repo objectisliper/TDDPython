c84e2f4c7ad88bf6ae5d3a0b35ab75c370b1b930
objectisliper <objectisliper@gmail.com>
2019-08-07 10:47:01 +0300

    Init

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,26 +11,13 @@ from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
 
 
-class ChromeWebdriver(webdriver.Chrome):
-
-    time_to_wait = 10
-
-    def find_element(self, by=By.ID, value=None):
-        start_time = time.time()
-        while True:
-            try:
-                return super().find_element(by, value)
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > self.time_to_wait:
-                    raise e
-                time.sleep(0.5)
-
-
 class NewVisitorTest(LiveServerTestCase):
     """ New user test """
 
+    time_to_wait = 10
+
     def setUp(self) -> None:
-        self.browser = ChromeWebdriver()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self) -> None:
         # User quit site
@@ -38,13 +25,20 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
+        start_time = time.time()
+        try:
+            table = self.browser.find_element_by_id('id_list_table')
 
-        rows = table.find_elements_by_tag_name('tr')
+            rows = table.find_elements_by_tag_name('tr')
 
-        self.assertIn(row_text, [row.text for row in rows])
+            self.assertIn(row_text, [row.text for row in rows])
+        except (AssertionError, WebDriverException) as e:
+            if time.time() - start_time > self.time_to_wait:
+                raise e
+            time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
+
         # Юзер заходит на сайт
 
         self.browser.get(self.live_server_url)
@@ -90,7 +84,52 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.wait_for_row_in_list_table('2: Buy smthng else')
 
-        self.fail('looolz')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+
+        # Start of new list
+        self.browser.get(self.live_server_url)
+
+        # Enter a name of point
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        inputbox.send_keys('Buy smthng else')
+
+        # Save changes
+
+        inputbox.send_keys(Keys.ENTER)
+
+        # Url of page was changed
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # New user was visit site
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # New user see clear first page
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy smthng else', page_text)
+
+        # Enter a name of point
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        inputbox.send_keys('Buy smthng else')
+
+        # Save changes
+
+        inputbox.send_keys(Keys.ENTER)
+
+        # Url of page was changed
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy smthng else', page_text)
 
 
 if __name__ == '__main__':
