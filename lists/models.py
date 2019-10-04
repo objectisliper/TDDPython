5afdb5d2635c7610b1dd52126d7800d1aae31d46
objectisliper <objectisliper@gmail.com>
2019-10-04 13:25:56 +0300

    chapter 11-12 - my small changes (cause book example is boring) + some other changes

--- lists/models.py
+++ lists/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
-# Create your models here.
+from django.urls import reverse
 
 
 class List(models.Model):
-    pass
+
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
 
 
 class Item(models.Model):
-
-    text = models.TextField()
+    text = models.TextField(default='')
     list = models.ForeignKey(List, models.CASCADE, null=True)
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import Item, List
+from django.core.exceptions import ValidationError
+from django.shortcuts import redirect, render
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -9,17 +9,29 @@ def home_page(request):
 
 def new_list(request):
     list_ = List.objects.create()
-
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
+    return redirect(list_)
 
 
 def view_list(request, list_id):
-    return render(request, 'list.html', {'list': List.objects.get(id=list_id)})
-
-
-def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
 
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
+    if request.method == 'POST':
+        try:
+
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(list_)
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list': list_, 'error': error})
